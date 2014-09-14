07d7d845d78d45944642d7956276fe64d3355b2a
William Tsay <willtsay@gmail.com>
2014-09-14 12:06:50 -0700

    morestuf

--- app/assets/javascripts/ng-app/controllers/notSet.js
+++ app/assets/javascripts/ng-app/controllers/notSet.js
@@ -1,6 +1,6 @@
-// angular.module('notSetApp')
-//   .controller('notSetCtrl', ['$scope', '$timeout', notSetCtrl])
+angular.module('notSetApp')
+  .controller('notSetCtrl', ['$scope', notSetCtrl])
 
-//   function notSetCtrl($scope,$timeout){
-//     $scope.hi = "hello world"
-//   }
+  function notSetCtrl($scope){
+    $scope.hi = "hello world"
+  }
