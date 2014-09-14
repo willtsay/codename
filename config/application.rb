fe96dac8b26c729bffb810daa64deef3e07116eb
William Tsay <willtsay@gmail.com>
2014-09-14 11:09:22 -0700

    updated databaseyml

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module GameSet
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets", "stylesheets")
     config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets","fonts")
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
